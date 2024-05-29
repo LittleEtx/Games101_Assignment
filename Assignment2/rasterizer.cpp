3f13c34c2e6841839adec0ec28cd0ae626c2d71d
LittleEtx <1968717506@qq.com>
2024-05-30 01:59:40 +0800

    use depth for Ass2 instead of negative

--- Assignment2/rasterizer.cpp
+++ Assignment2/rasterizer.cpp
@@ -135,12 +135,12 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
             float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
             z_interpolated *= w_reciprocal;
 
-            z_interpolated = 50 - z_interpolated;
+            auto depth = - z_interpolated;
             int index = get_index(x, y);
-            if (z_interpolated < depth_buf[index]) {
-                depth_buf[index] = z_interpolated;
+            if (depth < depth_buf[index]) {
+                depth_buf[index] = depth;
                 Eigen::Vector3f color = t.getColor();
-                set_pixel(Vector3f(x, y, z_interpolated), color);
+                set_pixel(Vector3f(x, y, depth), color);
             }
         }
     }

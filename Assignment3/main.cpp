983b5bee8369dd738891c42854e33c4184e5c974
LittleEtx <1968717506@qq.com>
2024-06-09 00:36:13 +0800

    Fix the translation matrix of Ass3

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -58,7 +58,7 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float z
     Eigen::Matrix4f translate;
     translate << 1, 0, 0, 0,
                  0, 1, 0, 0,
-                 0, 0, 1, zNear + zFar / 2,
+                 0, 0, 1, (zNear + zFar) / 2,
                  0, 0, 0, 1;
 
     float width = 2 * zNear * std::tan(eye_fov / 2);
